a763d509fceb99cd2f0ee042ebef881f367d0e29
NehaPoonia <poonianeha91@gmail.com>
2022-09-12 17:30:01 -0400

    added database

--- backend/database/dbConnector.js
+++ backend/database/dbConnector.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-    user: "root",
-    host: "localhost",
-    password: "pass123$",
-    database: "peershare",
+    user: "admin",
+    host: "peershare.cpijuvcfcbvk.eu-central-1.rds.amazonaws.com",
+    password: "peershare123",
+    database: "carsharing",
     port: "3306",
     multipleStatements: true
 });
