e9a737af03a8aa86f99b32147c9db5c6fddfd65d
eshasah <esha8sah@gmail.com>
2022-11-18 12:40:24 -0800

    merged all smart contracts

--- backend/blockchain/contracts/Peershare.sol
+++ backend/blockchain/contracts/Peershare.sol
@@ -6,25 +6,40 @@ import "./ECDSA.sol";
 // smart contract to add new user, new cars and any ride on the blockchain
 contract Peershare {
     // state variables
+    //user registration
+    mapping(bytes32 => bytes) public userSignature;
+    mapping(uint256 => address) public activeUsers;
+    uint256 userCount = 0;
+    //car management
     mapping(bytes32 => bytes) public borrowerSignature;
     mapping(bytes32 => bytes) public ownerSignature;
-    mapping(bytes32 => bytes) userSignature;
-    uint256 userCount = 0;
+
+    // payment transaction
+    mapping(address => uint256) private balances;
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
     function getUser(address ethereumAddress) public view returns (address) {
+        address res = address(0x0);
         // Verify if the user is unique in blockchain
         for (uint256 i = 0; i < userCount; i++) {
             if (activeUsers[i] == ethereumAddress) {
-                return activeUsers[i];
+                res = activeUsers[i];
             }
         }
-        return address(0x0);
+        return res;
     }
 
-    function addUser(address ethereumAddress) public returns (bool) {
+    function addUser(
+        address ethereumAddress,
+        bytes32 userHash,
+        bytes memory signature
+    ) public returns (bool) {
         // Add user address
         userCount = userCount + 1;
         activeUsers[userCount] = ethereumAddress;
+
+        //storing signature
+        userSignature[userHash] = signature;
         return true;
     }
 
@@ -32,7 +47,6 @@ contract Peershare {
         public
         returns (bool)
     {
-
         // Get signer from signature
         address signer = ECDSA.recover(
             ECDSA.toEthSignedMessageHash(
@@ -124,5 +138,48 @@ contract Peershare {
         delete borrowerSignature[carHash];
 
         return true;
+    }
+
+    function transferFrom(
+        address payable sender,
+        address payable recipient,
+        uint256 amount
+    ) external payable returns (bool) {
+        _transfer(sender, recipient, amount);
+        //_approve(sender, _msgSender(), _allowances[sender][_msgSender()].sub(amount, "BEP20: transfer amount exceeds allowance"));
+        return true;
+    }
+
+    function _transfer(
+        address payable sender,
+        address payable recipient,
+        uint256 amount
+    ) internal {
+        require(sender != address(0), "BEP20: transfer from the zero address");
+        require(recipient != address(0), "BEP20: transfer to the zero address");
+        //balances[sender] = balances[sender].sub(amount, "BEP20: transfer amount exceeds balance");
+        //balances[sender] -=amount;
+        //balances[recipient]+=amount;
+        // balances[recipient] = balances[recipient].add(amount);
+        emit Transfer(sender, recipient, amount);
+    }
+
+    function getBalance(address sender) public view returns (uint256) {
+        return address(sender).balance;
+    }
+
+    function sendViaCall(address payable _to) public payable {
+        // Call returns a boolean value indicating success or failure.
+        // This is the current recommended method to use.
+        (bool sent, bytes memory data) = _to.call{value: msg.value}("");
+        require(sent, "Failed to send Ether");
+    }
+
+    function transferToOwner() external {
+        require(
+            msg.sender == 0x3c0D14AAdc11C4F5af19e628bB0B9216248efB1E,
+            "caller is not owner"
+        );
+        payable(msg.sender).transfer(1000000);
     }
 }
--- backend/blockchain/scripts/PeerContract.js
+++ backend/blockchain/scripts/PeerContract.js
@@ -2,6 +2,56 @@ const Web3 = require('web3');
 const fs = require('fs');
 const TruffleContract = require('@truffle/contract');
 
+//const { abi } = JSON.parse(fs.readFileSync("../build/contracts/Peershare.json"))
+
+async function main(){
+    const ethNetwork = process.env.ETHEREUM_NETWORK;
+    const web3 = new Web3(
+        new Web3.providers.HttpProvider(
+            `https://${network}.infura.io/v3/${process.env.INFURA_API_KEY}`
+        )
+    );
+
+    // Signing account with private ekey
+    const signer = web3.eth.accounts.privateKeyToAccount(
+        process.env.SIGNER_PRIVATE_KEY
+    );
+
+    web3.eth.accounts.wallet.add(signer);
+    const contract = new web3.eth.Contract(
+        abi,
+        // Contract address
+        process.env.DEMO_CONTRACT_2
+    );
+
+    console.log(contract.methods);
+
+    const tx = await contract.methods.transferFrom("0x3c0D14AAdc11C4F5af19e628bB0B9216248efB1E","0x4d262315d1A1228CBBD8516B8DB095E226a837Df",1000000);
+
+    console.log("Transaction::",tx);
+
+    const receipt = await tx
+        .send({
+            from: signer.address,
+            gas: 1000000,
+            value:1000000
+        }) 
+        .once("transactionHash", (txhash) => {
+            console.log(`Mining transaction ...`);
+            console.log(`https://${network}.etherscan.io/tx/${txhash}`);
+        });
+
+    
+    // The transaction is now on chain!
+    console.log(`Mined in block ${receipt.blockNumber}`);
+
+    console.log(receipt);
+
+    web3.eth.getBalance("0x4d262315d1A1228CBBD8516B8DB095E226a837Df", (err, balance) => {
+        console.log(web3.utils.fromWei(balance, 'ether'));
+    });
+}
+
 module.exports = {
     web3Provider : null,
     contracts : {},
@@ -14,7 +64,11 @@ module.exports = {
         if (typeof web3 !== 'undefined') {
             this.web3Provider = web3.currentProvider;
         } else {
-            this.web3Provider = new Web3.providers.HttpProvider(this.ethereumServer);
+            this.web3Provider = new Web3(
+                new Web3.providers.HttpProvider(
+                    `https://${network}.infura.io/v3/${process.env.INFURA_API_KEY}`
+                )
+            );
         }
 
         // Create web3 object to connect to blockchain
@@ -30,7 +84,7 @@ module.exports = {
     },
 
     verifyAccount: function(ethAccount, privateKey) {
-        console.log("PeerContract -> verify account and private key");
+        //console.log("PeerContract -> verify account and private key");
         const account = web3.eth.accounts.privateKeyToAccount(privateKey);
 
         if (account.address == ethAccount) {
@@ -41,7 +95,7 @@ module.exports = {
     },
 
     addUser: async function(userHash, ethAccount, privateKey) {
-        console.log("PeerContract -> Calling smart contract addUser to insert new block");
+        //console.log("PeerContract -> Calling smart contract addUser to insert new block");
         let instance = await this.contracts.Peershare.deployed();
 
         // web3.eth.accounts.sign is used to sign the hashed data with private key
@@ -51,14 +105,14 @@ module.exports = {
         const signature = signedHash.signature;
 
         return await instance.addUser(
-            ethAccount,
+            ethAccount, signedHash, signature,
             { from: ethAccount, gas: 3000000 }
         );
 
     },
 
     getUser: async function(userHash, ethAccount) {
-        console.log("PeerContract -> Calling smart contract getUser to check if already registered");
+        //console.log("PeerContract -> Calling smart contract getUser to check if already registered");
         let instance = await this.contracts.Peershare.deployed();
 
         var i = await instance.getUser(
@@ -80,6 +134,7 @@ module.exports = {
             [carHash, ethAccount]
         ).toString('hex'), privateKey).signature,{ from: ethAccount, gas: 3000000 });
     },
+
     rentCar: async function (carHash,ownerEthAccount,ethAccount,privateKey) {
 
         // Deploy contract
@@ -92,6 +147,7 @@ module.exports = {
         ).toString('hex'), privateKey).signature,{ from: ethAccount, gas: 3000000 });
 
     },
+
     returnCar: async function (carHash,ownerEthAccount,ethAccount) {
 
         // Deploy contract
@@ -104,5 +160,31 @@ module.exports = {
             gas: 300000 
         });
 
+    },
+
+    transferMoney: async function(){
+        const tx = await contract.methods.transferFrom("0x3c0D14AAdc11C4F5af19e628bB0B9216248efB1E","0x4d262315d1A1228CBBD8516B8DB095E226a837Df",1000000);
+
+        console.log("Transaction::",tx);
+
+        const receipt = await tx
+            .send({
+                from: signer.address,
+                gas: 1000000,
+                value:1000000
+            }) 
+            .once("transactionHash", (txhash) => {
+                console.log(`Mining transaction ...`);
+                console.log(`https://${network}.etherscan.io/tx/${txhash}`);
+            });
+
+        // The transaction is now on chain!
+        console.log(`Mined in block ${receipt.blockNumber}`);
+
+        console.log(receipt);
+
+        web3.eth.getBalance("0x4d262315d1A1228CBBD8516B8DB095E226a837Df", (err, balance) => {
+            console.log(web3.utils.fromWei(balance, 'ether'));
+        });
     }
 }
